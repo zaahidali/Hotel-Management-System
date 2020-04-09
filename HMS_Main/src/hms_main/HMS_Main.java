883b1817086484127c00c8fe61017ba35e346876
Zahid Ali <37751705+zaahidali@users.noreply.github.com>
2020-04-09 22:01:08 +0500

    Add files via upload

---
+++ HMS_Main/src/hms_main/HMS_Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hms_main;
+
+/**
+ *
+ * @author mtari_000
+ */
+public class HMS_Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Checkout str=new Checkout();
+        str.setVisible(true);
+        
+        //    new Welcome_To_Hms().setVisible(true);
+          // Checkin str=new Checkin();
+        //str.setVisible(true);
+        
+    }
+    
+}
---
+++ HMS_Main/src/hms_main/jDateChooser1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hms_main;
+
+/**
+ *
+ * @author mtari_000
+ */
+class jDateChooser1 {
+    
+}
